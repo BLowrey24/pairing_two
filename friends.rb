4107613bb191e3a39bf30bb0cc5553be3e6584e9
Boston Lowrey <bostonlowrey@gmail.com>
2022-11-02 17:22:27 -0400

    Add location variable

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Boston", "Lowrey", "Dawson", "Timmons", "Arthur"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+location = ["Nashville", "Georgia", "London", "New York", "California"]
+location.pop
+puts location
